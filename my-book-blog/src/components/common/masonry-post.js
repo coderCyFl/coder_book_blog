140ac5cb0036494fac4e5511b708dcdac88cea1d
codercy <codercy1112@gmail.com>
2023-12-18 23:14:56 +0800

    Add Pagination

--- my-book-blog/src/components/common/masonry-post.js
+++ my-book-blog/src/components/common/masonry-post.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import { categoryColors } from './style';
+import TagRow from './';
 
 export default function MasonryPost({ post, tagsOnTop }) {
     // Check if post and post.categories are defined
     if (!post || !post.categories) {
-        return null; // Or handle the case where post or post.categories is undefined
+         // Or handle the case where post or post.categories is undefined
+        return null;
     }
 
-    const imageBackground = { backgroundImage: `url("${require(`../../assets/images/${post.image}`)}")` };
-    const style = { ...imageBackground, ...post.style };
 
+    const windowWidth = window.innerWidth
+    const imageBackground = { backgroundImage: `url("${require(`../../assets/images/${post.image}`)}")` };
+    const style =  windowWidth > 900 ? { ...imageBackground, ...post.style} : imageBackground ;
+    
     return (
         <a className="masonry-post overlay" style={style} href={post.link}>
-            <div className='image-text'>
-                <div className='tag-container'>
-                    {post.categories?.map((tag, ind) =>
-                        <span key={ind} className='tag' style={{ backgroundColor: categoryColors[tag] }}>
-                            {tag.toUpperCase()}
-                        </span>
-                    )}
+            <div className='image-text' style={{justifyContent: tagsOnTop? 'space-between' : 'flex-end'}}>
+                <div>
+                    <h2 className='image-title'>{post.title}</h2>
+                    <span className='image-date'>{post.date}</span>
                 </div>
-                <h2 className='image-title'>{post.title}</h2>
-                <span className='image-date'>{post.date}</span>
             </div>
         </a>
     );
 }
+
